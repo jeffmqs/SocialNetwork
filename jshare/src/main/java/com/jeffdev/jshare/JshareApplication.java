85d91425183bec0673b219de334e51881e3c67ee
Jeff Marques <129700391+jeffmqs@users.noreply.github.com>
2024-09-22 16:39:14 -0300

    Adicionando arquivos

---
+++ jshare/src/main/java/com/jeffdev/jshare/JshareApplication.java
@@ -0,0 +1,13 @@
+package com.jeffdev.jshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JshareApplication.class, args);
+	}
+
+}
---
+++ jshare/src/test/java/com/jeffdev/jshare/JshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffdev.jshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
